0420016d3f2ba5bbfed79ea5abea0688c693b17c
iTBoyer <iTBoyer@qq.com>
2021-12-07 20:00:58 +0800

    update

--- swift/main.swift
+++ swift/main.swift
@@ -7,13 +7,12 @@
 
 import Foundation
 import Fastlane
-import CmdLib
-let ipFiles = ipFile()
-print("Hello, world!")
 
 //let iuooooPath = "/Users/boyer/Desktop/result-urls.json"
 //let projDics = ipFiles.loadJsonData(path: iuooooPath, proj: "JHBluetoothLibrary")
 //print(projDics)
 //ipFiles.fetchSource()
 
+//http://docs.fastlane.tools/getting-started/ios/fastlane-swift/#getting-started-with-fastlaneswift-beta
+//Add an entry point (@main) or a main.swift file (mandatory for executable SPM packages) and don't forget to start the fastlane runloop as follows:
 Main().run(with: Fastfile())
