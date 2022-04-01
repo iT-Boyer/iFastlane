555d1768b5c563c7be84ccb2d03be94a1fdd0cdf
iTBoyer <iTBoyer@qq.com>
2022-04-01 18:34:40 +0800

    update

--- Gemfile
+++
@@ -1,5 +0,0 @@
-# More information about how to use this file in [CONTRIBUTING.md](CONTRIBUTING.md)
-
-source "https://rubygems.org"
-
-gemspec path: File.expand_path("~/hsg/fastlane")
--- swift/Lib/fastlane+JHBase.swift
+++ swift/Lib/fastlane+JHBase.swift
@@ -6,8 +6,25 @@
 //
 
 import Foundation
+import Fastlane
 
 extension Fastfile
 {
-    
+    //runner lane jhbase name libname
+    func jhbaseLane(withOptions options:[String: String]?) {
+        
+        guard let name = options?["name"], name.count > 0 else {
+            echo(message: "非法地址")
+            return
+        }
+        
+        spm(command: "build",
+            buildPath: "~/Desktop/spm_build",
+            packagePath: "~/hsg/\(name)",
+            configuration: "release",
+            disableSandbox: true,
+            xcprettyOutput: "simple",
+            xcprettyArgs: "–tap –no-utf",
+            verbose: false)
+    }
 }
