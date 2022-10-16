a042bad7975e2aa294a225835ba17d6da13f7661
iTBoyer <iTBoyer@qq.com>
2022-10-16 21:52:56 +0800

    现象：新增通道lane，可以在web段运行ios app 原因： 处理办法：

---
+++ swift/tools/Appetize.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by boyer on 2022/10/16.
+//
+
+import Foundation
+import Fastlane
+//https://appetize.io
+extension Fastfile
+{
+    
+    func appetizeLane(){
+        
+        appetize(apiToken: "tok_wkvivs35obwvkt4pmpyaavj6gu")
+    }
+}
