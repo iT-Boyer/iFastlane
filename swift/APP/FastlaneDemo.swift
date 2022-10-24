2edb33331e1a8e4a12d2273bfad02937db0f9c4a
iTBoyer <iTBoyer@qq.com>
2022-10-24 15:11:40 +0800

    现象：模拟blink打包方式上传ipa包 原因： 处理办法：

--- swift/APP/FastlaneDemo.swift
+++ swift/APP/FastlaneDemo.swift
@@ -33,17 +33,56 @@ extension Fastfile
             outputDirectory: "./build",
             configuration: "Debug",
             exportMethod: "development",
-            exportXcargs: "-allowProvisioningUpdates" //开启自动签名，xcodebuild默认禁用
-//            sdk: .userDefined(iossdk)
+            exportXcargs: "-allowProvisioningUpdates", //开启自动签名，xcodebuild默认禁用
+           sdk: .userDefined("iphoneos")
         )
         
 //        installOnDevice(deviceId: ipad, ipa: ipaPath)
 //        uploadPgyer() //发布到蒲公英
 //        iPappLane()   //发布到github
+        sh(command: "pwd", log: .userDefined(true))
         appetizeLane(path: "./build/FastlaneDemo.ipa")
     }
 
-    
+
+    func blinkBuildLane() {
+        desc("使用blink的编译方式打包测试")
+ //   run: set -o pipefail && xcodebuild archive -project FastlaneDemo.xcodeproj -scheme FastlaneDemo -sdk iphoneos -configuration Debug clean build IPHONEOS_DEPLOYMENT_TARGET='15.0' CODE_SIGN_IDENTITY='' CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO ONLY_ACTIVE_ARCH=NO | tee build.log | xcpretty
+        gym(project: "FastlaneDemo.xcodeproj",
+            scheme: "FastlaneDemo",
+            clean: true,
+            configuration: "Debug",
+            exportMethod: "package",
+            //skipArchive: .userDefined(true), //跳过打包无法生成app/ipa文件
+            skipCodesigning: .userDefined(true), //跳过签名
+            buildPath: "./build",
+            sdk: "iphoneos"
+            ,xcargs: "ONLY_ACTIVE_ARCH=NO"  //添加额外字段，通过xcodebuild --help支持的字段
+        )
+
+        let shcmd = """
+            cd build    \
+            && mkdir Payload \
+            && mv ./*/Products/Applications/FastlaneDemo.app Payload/ \
+            && zip -r FastlaneDemo.ipa Payload \
+            && pwd \
+            && cd .. \
+            && pwd
+            """
+        let shhcmd = """
+            mkdir Payload
+            mv build/*/Products/Applications/FastlaneDemo.app Payload/
+            zip -r FastlaneDemo.ipa Payload
+"""
+        
+        let cmdlog = sh(command: shcmd, log: .userDefined(true)) { err in
+            print("错误状态：\(err)")
+        }
+            
+        log(message: "开始上传iPA:\(cmdlog)")
+//        appetize(apiToken: "tok_wkvivs35obwvkt4pmpyaavj6gu", path: "FastlaneDemo.ipa")
+    }
+
     func swiftSimLane() {
         
 //        desc("在模拟器上运行")
