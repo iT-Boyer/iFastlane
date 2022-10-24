8e29f72222e82fc2098e146e2094397973bd74f5
iTBoyer <iTBoyer@qq.com>
2022-10-24 15:47:09 +0800

    现象：添加参数的上传到apptize 原因： 处理办法：

--- swift/tools/Appetize.swift
+++ swift/tools/Appetize.swift
@@ -11,8 +11,18 @@ import Fastlane
 extension Fastfile
 {
     
-    func appetizeLane(path:String){
-        
+    /** 支持带参数的lane 升级之后 使用空格 来拼接key:value
+        调用语法：fastlane [lane] key value key2 value2
+        终端调用：fastlane  appetize  path ../path/app.ipa
+     https://docs.fastlane.tools/getting-started/ios/fastlane-swift/
+     */
+    func appetizeLane(withOptions options:[String: String]?) {
+        if let path = options?["path"]{
+            appetizeLane(path: path)
+        }
+    }
+    
+    func appetizeLane(path:String) {
         appetize(apiToken: "tok_wkvivs35obwvkt4pmpyaavj6gu",path: .userDefined(path))
     }
 }
